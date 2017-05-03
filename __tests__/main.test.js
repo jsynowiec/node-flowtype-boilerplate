5c19e18144feae62a7b8ae8521590c35f7598ccb
phra <greensoncio@gmail.com>
2017-05-03 23:38:33 +0100

    fix setTimeout call and tests, fixes #1

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -2,20 +2,20 @@
 
 import greeter from '../src/main';
 
-// https://facebook.github.io/jest/docs/api.html#jestusefaketimers
 jest.useFakeTimers();
 
-let hello;
-
-beforeAll(async () => {
-  hello = await greeter('John');
-});
-
-it('delays the greeting by 2 seconds', () => {
+it('delays the greeting by 2 seconds', async () => {
+  expect.assertions(2);
+  const promise = greeter('John');
+  jest.runOnlyPendingTimers();
+  await promise;
   expect(setTimeout.mock.calls.length).toBe(1);
   expect(setTimeout.mock.calls[0][1]).toBe(2000);
 });
 
-it('greets a user with `Hello, {name}` message', () => {
+it('greets a user with `Hello, {name}` message', async () => {
+  const promise = greeter('John');
+  jest.runOnlyPendingTimers();
+  const hello = await promise;
   expect(hello).toBe('Hello, John');
 });

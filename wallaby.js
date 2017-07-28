5f566c119a43cba46b475efca359a0412263d0fb
Byron Sommardahl <byron@acklenavenue.com>
2017-07-28 09:30:27 -0700

    wallaby working in vscode.

--- wallaby.js
+++ wallaby.js
@@ -1,21 +1,13 @@
 /* eslint-disable global-require */
 
 module.exports = (wallaby) => ({
-  files: [
-    'src/**/*.js',
-  ],
-
-  tests: [
-    '__tests__/**/*.test.js',
-  ],
-
+  files: ['src/**/*.js'],
+  tests: ['__tests__/**/*.test.js'],
   env: {
     type: 'node',
   },
-
   compilers: {
     '**/*.js': wallaby.compilers.babel({ babel: require('babel-core') }),
   },
-
   testFramework: 'jest',
 });

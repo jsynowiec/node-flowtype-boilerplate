de0104160add891954ae778489709ba861724e4f
Camilo Rivera <camilo@riverapineda.com>
2017-10-23 16:04:27 -0600

    adding cloudformation

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 set -eo pipefail
+echo "--- Copy Templates"
+cp -v src/config/loggly.js.sample src/config/loggly.js
+echo "--- Build"
+if [[ "$BUILDKITE_BRANCH" == "master"  ]]; then
+export NODE_ENV=prod
+fi
+npm install
+if [[ "$BUILDKITE_BRANCH" == "master"  ]]; then
+export NODE_ENV=production
+fi
+npm run build
 echo "--- Deploy to $BUILDKITE_BRANCH"
+gulp deploy
+buildkite-agent artifact upload "zip/**/*.zip"

a4ec0f173b6dc656859d3e243c5a5b4ab8a79e16
Camilo Rivera <camilo@riverapineda.com>
2017-10-23 12:17:51 -0600

    add sonarqube to glee

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -5,10 +5,10 @@ echo "--- Test"
 npm install
 npm run build
 npm run test
-#if [ $BUILDKITE_PULL_REQUEST = false ]; then
-#    echo "--- Sonar Scanner: Scanning "$BUILDKITE_BRANCH" branch"
-#    sonar-scanner
-#else
-#    echo "--- Sonar Scanner: Scanning pull request on "$BUILDKITE_BRANCH" branch"
-#    sonar-scanner -Dsonar.analysis.mode=preview   -Dsonar.github.pullRequest=$BUILDKITE_PULL_REQUEST  -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN | tee bk-pipeline.log
-#fi
+if [ $BUILDKITE_PULL_REQUEST = false ]; then
+    echo "--- Sonar Scanner: Scanning "$BUILDKITE_BRANCH" branch"
+    sonar-scanner
+else
+    echo "--- Sonar Scanner: Scanning pull request on "$BUILDKITE_BRANCH" branch"
+    sonar-scanner -Dsonar.analysis.mode=preview   -Dsonar.github.pullRequest=$BUILDKITE_PULL_REQUEST  -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN | tee bk-pipeline.log
+fi

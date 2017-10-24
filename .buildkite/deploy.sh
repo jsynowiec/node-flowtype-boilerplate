196cf3765f6a1dcc45f380d1503cc323dc7396d7
Camilo Rivera <camilo@riverapineda.com>
2017-10-23 20:17:38 -0600

    move install to container

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 set -eo pipefail
-echo "--- Copy Templates"
-cp -v src/config/loggly.js.sample src/config/loggly.js
-cp -v src/config/sequelize.js.sample src/config/sequelize.js
 echo "--- Build"
-if [[ "$BUILDKITE_BRANCH" == "master"  ]]; then
-export NODE_ENV=prod
-fi
-npm install
-if [[ "$BUILDKITE_BRANCH" == "master"  ]]; then
-export NODE_ENV=production
-fi
 npm run build
 echo "--- Deploy to $BUILDKITE_BRANCH"
 gulp deploy

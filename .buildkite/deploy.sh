20fa216e9a92c4190c0086644930d5280babda29
Camilo Rivera <camilo@riverapineda.com>
2017-10-23 21:46:30 -0600

    moving templates to dockerfile

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 set -eo pipefail
-echo "--- Copy Templates"
-cp -v src/config/loggly.js.sample src/config/loggly.js
-cp -v src/config/sequelize.js.sample src/config/loggly.js
 echo "--- Build"
 npm run build
 echo "--- Deploy to $BUILDKITE_BRANCH"

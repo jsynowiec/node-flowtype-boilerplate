24909a2a14a866bc1d19189a5141406800a9f655
Camilo Rivera <camilo@riverapineda.com>
2017-12-21 01:51:52 -0600

    test ecr

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -2,5 +2,6 @@
 
 set -eo pipefail
 echo "--- Build"
-docker build -f dockerfile-deploy -t glee:$APPVERSION .
-docker images
+docker build -f dockerfile-deploy -t $APPNAME:$APPVERSION .
+docker tag $APPNAME 487799950875.dkr.ecr.us-east-1.amazonaws.com/$APPNAME:$APPVERSION
+docker push 487799950875.dkr.ecr.us-east-1.amazonaws.com/$APPNAME:$APPVERSION
